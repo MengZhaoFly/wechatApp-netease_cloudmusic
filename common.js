d3298b55693a837bde7f0427ea23214aeee938da
MengZhaoFly <1424254461@qq.com>
2017-05-11 14:14:17 +0800

    Update common.js

--- common.js
+++ common.js
@@ -26,7 +26,6 @@ const operation = {
                 }
             })
         })
-
     },
     asyncGetStorage: function (key) {
         return new Promise((resolve, reject) => {
@@ -35,8 +34,48 @@ const operation = {
                 success: function (res) {
                     resolve(res.data)
                 },
-                fail: function (err){
+                fail: function (err) {
                     reject(err)
+                }
+            })
+        })
+    },
+    getlyric: function (id) {
+        return new Promise((resolve, reject) => {
+            console.log('id:', id)
+            let url = `http://neteasemusic.leanapp.cn/lyric`
+            wx.request({
+                url: url,
+                data: {
+                    id: id
+                },
+                method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+                // header: {}, // 设置请求的 header
+                success: function (res) {
+                    // success
+                    if (!res.data.lrc.lyric) return false;
+                    let lyric = res.data.lrc.lyric
+                    let timearr = lyric.split('[')
+                    let obj = {}
+                    let lyricArr = []
+                    // seek 为键  歌词为value
+                    timearr.forEach((item) => {
+                        let key = parseInt(item.split(']')[0].split(':')[0]) * 60 + parseInt(item.split(']')[0].split(':')[1])
+                        let val = item.split(']')[1]
+                        obj[key] = val
+                    })
+                    for (let key in obj) {
+                        // obj[key] = obj[key].split('\n')[0]
+                        lyricArr.push(obj[key])
+                    }
+                    // cb && cb(obj, lyricArr)
+                    resolve(lyricArr)
+                },
+                fail: function (err) {
+                    reject(err)
+                },
+                complete: function (res) {
+                    // complete
                 }
             })
         })
